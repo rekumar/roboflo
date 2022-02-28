149c35d71e2048a621b422e97b8c5e91ed686d01
Rishi Kumar <rishi42@gmail.com>
2022-02-27 17:54:22 -0800

    remove incorrect required package from setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(os.path.join(this_dir, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="roboflo",
-    version="0.1",
+    version="0.1.1",
     description="Scheduler for automation tasks that involve multiple stations/workers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -36,7 +36,6 @@ setup(
         "numpy",
         "ortools",
         "matplotlib",
-        "json",
     ],
     # extras_require={
     #     'model_saving': ['h5py'],
